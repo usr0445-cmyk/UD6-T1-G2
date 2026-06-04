5c738431f5b1daed6606b1f2ae3fef574fe248fb
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-04 10:52:37 +0100

    Ponemos los atributos necesarios para la clase, y creamos el constructor de la clase para poder crear objetos de tipo participante

--- src/net/salesianoslacuesta/Participante/participante.java
+++ src/net/salesianoslacuesta/Participante/participante.java
@@ -2,4 +2,13 @@ package net.salesianoslacuesta.Participante;
 
 public class participante {
     
+    private String idJugador;
+    private String nombre;
+    private String categoria;
+
+    public participante(String idJugador, String nombre, String categoria) {
+        this.idJugador = idJugador;
+        this.nombre = nombre;
+        this.categoria = categoria;
+    }
 }
