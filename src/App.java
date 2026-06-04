2d803bed0715361aa38ba696766e3ca4cca21aed
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-04 10:19:19 +0100

    Con este cola.add añadimos pedidos a la cola, cada pedido es un objeto de la clase pedido, añadimos el import de su clase tambien

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import net.salesianoslacuesta.Pedido.*;
 
 public class App {
     public static void main(String[] args) {
 
-        Queue<String> cola = new LinkedList<>();
+        Queue<pedido> cola = new LinkedList<>();
+
+        cola.add(new pedido("P001", "Carla", "Cafe con leche", 1));
     }
 
 }
