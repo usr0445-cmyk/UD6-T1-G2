93b704bfe17ccd01b3a62af6554cca2f7ad9bc88
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-04 09:27:18 +0100

    He añadido los atributos necesarios para la clase

--- src/net/salesianoslacuesta/Pedido/pedido.java
+++ src/net/salesianoslacuesta/Pedido/pedido.java
@@ -2,4 +2,9 @@ package net.salesianoslacuesta.Pedido;
 
 public class pedido {
     
+    private String codigo;
+    private String cliente;
+    private String producto;
+    private boolean paraLlevar;
+
 }
