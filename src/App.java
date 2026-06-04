573c3e6647a761b39cf210489def75fab1d5e7a4
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-04 10:23:11 +0100

    Añadimos los .add restantes con los datos que nos dio el ejercicio

--- src/App.java
+++ src/App.java
@@ -9,6 +9,10 @@ public class App {
         Queue<pedido> cola = new LinkedList<>();
 
         cola.add(new pedido("P001", "Carla", "Cafe con leche", 1));
+        cola.add(new pedido("P002", "Mario", "Té verde", 0));
+        cola.add(new pedido("P003", "Nerea", "Zumo natural", 1));
+        cola.add(new pedido("P004", "Ivan", "Tostada integral", 0));
+        cola.add(new pedido("P005", "Laura", "Te verde", 1));
     }
 
 }
