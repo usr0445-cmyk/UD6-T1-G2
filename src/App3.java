0459e5161ff5d7916dfd65092caca5d21eac24da
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-05 10:33:58 +0100

    Añadido comentarios

--- src/App3.java
+++ src/App3.java
@@ -1,6 +1,10 @@
+// Files permite leer y escribir ficheros usando métodos simples
 import java.nio.file.Files;
+// Path representa la ruta del fichero
 import java.nio.file.Path;
 import java.util.ArrayList;
+
+// List se usa para almacenar las líneas del CSV 
 import java.util.List;
  
 public class App3 {
