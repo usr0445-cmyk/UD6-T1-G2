d21eed2aad283ba1c090c287e21de1d26dda1061
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-05 08:53:06 +0100

    Se pone un hashset de participantes para almacenar los participantes de un torneo, se añade un comentario explicando un poco lo que hace

--- src/App2.java
+++ src/App2.java
@@ -1,6 +1,13 @@
+//Estos imports son necesarios para usar las clases HashSet y HashMap en el codigo
 import java.util.HashSet;
 import java.util.HashMap;
+import net.salesianoslacuesta.Participante.*;
 
  public class App2 {
+    public static void main(String[] args) {
+
+ // Aqui usamos HashSet que es para almacenar objetos de tipo participante sin permitir duplicados, no se pueden agregar dos participantes con el mismo idJugador
+    HashSet<participante> participantes = new HashSet<>();
     
+    }
 }
