46f4feaf0d95670c69821d9fd5ad7e85396c1ea2
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-05 09:55:54 +0100

    Ponemos los atributos necesarios y el constructor

--- src/net/salesianoslacuesta/Pelicula/pelicula.java
+++ src/net/salesianoslacuesta/Pelicula/pelicula.java
@@ -2,4 +2,13 @@ package net.salesianoslacuesta.Pelicula;
 
 public class pelicula {
     
+    private String titulo;
+    private String genero;
+    private int duracionMinutos;
+
+    public pelicula (String titulo, String genero, int duracionMinutos) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracionMinutos = duracionMinutos;
+    }
 }
