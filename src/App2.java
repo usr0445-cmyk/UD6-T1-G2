1a6446cb161e7481e36e47b3ab89a64fa350caa6
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-05 09:15:12 +0100

    Ponemos dos System mostrando el numero de intentos de añadir participantes y el numero real de participantes registrados

--- src/App2.java
+++ src/App2.java
@@ -32,13 +32,11 @@ import net.salesianoslacuesta.Participante.*;
     for (participante participante : participantes) { //Usamos for para para recorrer el HashSet de participantes
         System.out.println(participante);
 
-
-
-
-
-
-
-
+    // Los duplicados no se añaden porque HashSet usa equals() y hashCode para evitar elementos repetidos
+    System.out.println("Intentos de añadir: " + intentos);
+    System.out.println("Participantes reales: " + participantes.size());
 
     }
+}
+
 }
