39f46ac18f5fd6048277ea7ed286883c9bf1abd7
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-06-05 13:59:58 +0100

    Aqui creamos pelicula para guardar la linea completa como un pelicula

--- src/App3.java
+++ src/App3.java
@@ -1,10 +1,10 @@
 // Files permite leer y escribir ficheros usando metodos simples
 import java.nio.file.Files;
-// Path representa la ruta del fichero
+//Path representa la ruta del fichero
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-// List se usa para almacenar las líneas del CSV 
+//List se usa para almacenar las líneas del CSV 
 import java.util.List;
 import net.salesianoslacuesta.Pelicula.*;
 
@@ -15,14 +15,18 @@ public class App3 {
         ArrayList<pelicula> lista = new ArrayList<>();
 
         try {
-            // Leemos todas las lineas del CSV
+            //Leemos todas las lineas del CSV
             List<String> lineas = Files.readAllLines(Path.of("data/peliculas.csv"));
 
+            //Ignoramos la primera linea
             for (int i = 1; i < lineas.size(); i++) {
                 String linea = lineas.get(i);
 
                 int coma1 = linea.indexOf(",");
                 int coma2 = linea.indexOf(",");
+
+                pelicula pelicula = new Pelicula(linea);
+                lista.add(pelicula);
                 
             }
 
